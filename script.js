579fd1f2222e343e0d5350232eb31b897b151371
Eris Kociaj <eris.kociaj@elev.ga.ntig.se>
2023-04-25 12:55:29 +0200

    Avklarad kod

---
+++ script.js
@@ -0,0 +1,8 @@
+menu = document.querySelector("#menu");
+navigation_bar = document.querySelector(".navigation_bar");
+ 
+
+function cart() {
+    const nav = document.querySelector(".navigation_bar");
+    nav.classList.toggle("active");
+}
